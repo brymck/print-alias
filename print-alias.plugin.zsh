2ad72638aff8bb4373126a5cc4a6549b876215a7
Bryan McKelvey <bryan.mckelvey@gmail.com>
2019-11-12 18:14:54 +0900

    Move constants inline so they don't get overwritten

--- print-alias.plugin.zsh
+++ print-alias.plugin.zsh
@@ -1,11 +1,14 @@
-local -r ALIAS_FORMAT=$'\e[36m'
-local -r NON_ALIAS_FORMAT=$'\e[2m'
-local -r RESET_COLORS=$'\e[0m'
-
 _print-alias-accept-line () {
     emulate -L zsh
+
+    # Constants
+    local -r ALIAS_FORMAT=$'\e[36m'
+    local -r NON_ALIAS_FORMAT=$'\e[2m'
+    local -r RESET_COLORS=$'\e[0m'
+
     local -a words
     words=( ${(z)BUFFER} )
+
     local -r first_word=${words[1]}
     if [[ "$(whence -w $first_word 2>/dev/null)" == "${first_word}: alias" ]]; then
         shift words
