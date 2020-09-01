215b81b58010ed83a90f5827515fdfc44c019506
Bryan McKelvey <bryan.mckelvey@gmail.com>
2020-09-01 16:37:16 +0900

    Add tests

--- print-alias.plugin.zsh
+++ print-alias.plugin.zsh
@@ -1,9 +1,8 @@
-_print-alias-accept-line () {
-    emulate -L zsh
-
+_print-alias () {
     # Constants
+    local -r PREFIX=${PRINT_ALIAS_PREFIX:-}
     local -r ALIAS_FORMAT=${PRINT_ALIAS_FORMAT:-$'\e[36m'}
-    local -r NON_ALIAS_FORMAT=$'\e[2m'
+    local -r NON_ALIAS_FORMAT=${PRINT_NON_ALIAS_FORMAT:-$'\e[2m'}
     local -r RESET_COLORS=$'\e[0m'
 
     local -a words
@@ -12,11 +11,17 @@ _print-alias-accept-line () {
     local -r first_word=${words[1]}
     if [[ "$(whence -w $first_word 2>/dev/null)" == "${first_word}: alias" ]]; then
         shift words
-        echo -nE $'\n'"${ALIAS_FORMAT}$(whence $first_word)${RESET_COLORS}"
+        echo -nE $'\n'"${PREFIX}${ALIAS_FORMAT}$(whence $first_word)${RESET_COLORS}"
         for word in $words; do
             echo -nE " ${NON_ALIAS_FORMAT}${word}${RESET_COLORS}"
         done
     fi
+}
+
+zle -N accept-line _print-alias-accept-line
+_print-alias-accept-line () {
+    emulate -L zsh
+    _print-alias
     zle .accept-line
 }
 
