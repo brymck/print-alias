8997efc356c829f21db271424fbc8986a7203119
Alexandre Feblot <afeblot@users.noreply.github.com>
2020-11-05 15:21:04 +0100

    Add exclusion support (#8)

--- print-alias.plugin.zsh
+++ print-alias.plugin.zsh
@@ -4,14 +4,26 @@ _print-alias () {
     local -r ALIAS_FORMAT=${PRINT_ALIAS_FORMAT:-$'\e[36m'}
     local -r NON_ALIAS_FORMAT=${PRINT_NON_ALIAS_FORMAT:-$'\e[2m'}
     local -r RESET_COLORS=$'\e[0m'
+    local -r IGNORE_ALIASES=(${PRINT_ALIAS_IGNORE_ALIASES:-})
+    local -r IGNORE_REDEFINED_COMMANDS=${PRINT_ALIAS_IGNORE_REDEFINED_COMMANDS:-'false'}
 
     local -a words
     words=( ${(z)BUFFER} )
 
     local -r first_word=${words[1]}
+    if [[ ${IGNORE_ALIASES[(ie)$first_word]} -le ${#IGNORE_ALIASES} ]]; then
+        return
+    fi
+
     if [[ "$(whence -w $first_word 2>/dev/null)" == "${first_word}: alias" ]]; then
         shift words
-        echo -nE $'\n'"${PREFIX}${ALIAS_FORMAT}$(whence $first_word)${RESET_COLORS}"
+        local -r aliased_command=($(whence $first_word))
+        if [[ $IGNORE_REDEFINED_COMMANDS == 'true' ]]; then
+            if [[ $first_word == ${aliased_command[1]} ]]; then
+                return
+            fi
+        fi
+        echo -nE $'\n'"${PREFIX}${ALIAS_FORMAT}${aliased_command}${RESET_COLORS}"
         for word in $words; do
             echo -nE " ${NON_ALIAS_FORMAT}${word}${RESET_COLORS}"
         done
